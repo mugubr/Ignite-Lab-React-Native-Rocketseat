4fd01a85e2af71c1c335f99d9ed290bfef85367d
mugubr <miguimuniz@gmail.com>
2022-07-22 14:49:25 -0300

    Aplicação finalizada

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,25 +1,53 @@
-import { useState } from 'react';
-import { VStack, Heading, Icon, useTheme } from 'native-base';
-import { Envelope, Key } from 'phosphor-react-native';
+import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { Alert } from "react-native";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
 
-import Logo from '../assets/logo_primary.svg';
+import Logo from "../assets/logo_primary.svg";
 
-import { Input } from '../components/Input';
-import { Button } from '../components/Button';
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log(name, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informar e-mail e senha ");
+    }
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido");
+        }
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida");
+        }
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar");
+      });
   }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      {/* <Logo /> */}
+      <Logo />
 
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
@@ -28,8 +56,10 @@ export function SignIn() {
       <Input
         mb={4}
         placeholder="E-mail"
-        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        onChangeText={setName}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
       />
 
       <Input
@@ -40,7 +70,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
-  )
+  );
 }
